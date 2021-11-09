861511b6177490fb845c87e3f0c50e5c15bb9b55
jncv7 <jcv7@njit.edu>
2021-11-08 23:24:37 -0500

    comfirmed that all insrts work. also able to test with jncvserver and test_client

--- Yoink/include/inserts.php
+++ Yoink/include/inserts.php
@@ -3,6 +3,16 @@
 /*
     JNCV:
         put all the functions that insert into the tables in here
+	// NOV 11/08 -> What I know is tested and works now:
+		- register 
+		- setMovie
+		- addSaved
+		- addReview
+		- addFrom
+		- setSession
+
+	-> what doesnt work?
+
 */
 
 /*
@@ -25,120 +35,143 @@ function testfoo($username, $password){ // testing inserting stuff LOL?
 */
 
 // insert a new user into the uesrLogin table
-function register ($username, $password, $email){
-	try{
+function register($username, $password, $email)
+{
+	try {
 
 		require 'config.php'; // to get the sensitive stuff
-		$db = new PDO ("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword);
+		$db = new PDO("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword);
 
-		$insert = $db->prepare ("INSERT INTO `userLogin` (username, password, email) values (:username, :password, :email)");
-		$v = array (":username"=>$username, ":password"=>$password, ":email"=>$email);
+		$insert = $db->prepare("INSERT INTO `userLogin` (username, password, email) values (:username, :password, :email)");
+		$v = array(":username" => $username, ":password" => $password, ":email" => $email);
 		$insert->execute($v);
-	}catch (Exception $e){
-			echo "this is quite unfortunate. You cant add users at the momment. :(" . $e->getMessage();
-
+	} catch (Exception $e) {
+		echo "this is quite unfortunate. You cant add users at the momment. :(" . $e->getMessage();
 	}
-
-
-
 }
 
 //insert into the reivews table
-function addReview ($userID, $movieID, $ratingReview, $reviewText){
+// comfrimed to work now :)
+function addReview($movieID, $userID, $ratingReview, $reviewText)
+{
 
-	try{
+	try {
 		require 'config.php'; // to get the sensitive stuff
-		$db = new PDO ("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword); // get into db
-		$rInsert = $db->prepare("INSERT INTO `reviews` (userID, movie_id, reviewRating, reviewText) values (:userID, :movieID, :ratingReview, :reviewText");
-		$varis = array (":userID"=>$userID, ":movie_id"=>$movieID,":reviewRating"=>$ratingReview, ":reviewText"=>$reviewText);
-		$rInsert->execute($varis);
-		echo "insert into reivew complete".PHP_EOL;
-	}catch(Exception $e){
-		echo "you were not able to insert a reivew into the table". $e->getMessage ().PHP_EOL;
-
+		$db = new PDO("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword); // get into db
+		$rInsert = $db->prepare("
+		INSERT INTO reviews (`movie_id`, `userID`, `reviewRating`, `reviewText`) 
+		values (:movieID, :userID, :rr, :rt)");
+		$v = array(":movieID" => $movieID, ":userID" => $userID, ":rr" => $ratingReview, ":rt" => $reviewText);
+		$rInsert->execute($v);
+		// echo "insert into reivew complete" . PHP_EOL;
+	} catch (Exception $e) {
+		echo "you were not able to insert a reivew into the table" . $e->getMessage() . PHP_EOL;
 	}
-
 }
 
 
 // add movies to the userInv
-function addSaved ($userID, $movieID){
+// comfirmed working as long as there are users and movies that exist :)
+function addSaved($userID, $movieID)
+{
 
 	try {
 
 		require 'config.php'; // to get the sensitive stuff
-		$db = new PDO ("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword); // get into db
+		$db = new PDO("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword); // get into db
 
-		$rInsert = $db->prepare("INSERT INTO `userInv` (userID, movie_id) values (:userID, :movieID");
-		$varis = array (":userID"=>$userID, ":movie_id"=>$movieID);
+		$rInsert = $db->prepare("INSERT INTO `userInv` (`movie_id`, `userID`) 
+		values (:md,:ud)");
+		$varis = array(":md" => $movieID, ":ud" => $userID);
 		$rInsert->execute($varis);
 
 		//echo "insert into reivew complete".PHP_EOL;
-		
-	} catch (Exception $e) {
-		echo "you wre not able to put the movie into the userINV ". $e->getMessage(). PHP_EOL;	
-	}
 
+	} catch (Exception $e) {
+		echo "you wre not able to put the movie into the userINV " . $e->getMessage() . PHP_EOL;
+	}
 }
 
 // adding movies to the uersINv
-function addMovie ($userID, $movieID){
+// lol is this justnoather addsaved? oops
+function addMovie($userID, $movieID)
+{
 
 	try {
 		require 'config.php'; // to get the sensitive stuff
-		$db = new PDO ("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword); // get into db
-		
-		$rInsert = $db->prepare("INSERT INTO `userInv` (userID, movie_id) values (:userID, :movieID)");
-		$varis = array (":userID"=>$userID, ":movie_id"=>$movieID);
-		$rInsert->execute($varis);
-		
-		echo "insert into reivew complete".PHP_EOL;
-		
-	} catch (Exception $e) {
-		echo "you wre not able to put the movie into the movies ". $e->getMessage(). PHP_EOL;	
-	}
+		$db = new PDO("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword); // get into db
 
+		$rInsert = $db->prepare("INSERT INTO `userInv` (userID, movie_id) values (:userID, :movieID)");
+		$varis = array(":userID" => $userID, ":movie_id" => $movieID);
+		$rInsert->execute($varis);
+
+		echo "insert into reivew complete" . PHP_EOL;
+	} catch (Exception $e) {
+		echo "you wre not able to put the movie into the movies " . $e->getMessage() . PHP_EOL;
+	}
 }
 
 // adding stuff to the table for preferences (ie formTable)
-function addForm ($userID, $genre, $duration, $year, $language){
+function addForm($userID, $genre, $duration, $year, $language)
+{
 	try {
 		require 'config.php'; // to get the sensitive stuff
-		$db = new PDO ("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword); // get into db
-		
-		$fInsert = $db->prepare("INSERT INTO `formTable` (userID, genre, duration, `year`, language) values (:userID, :genre, :duration, `:year`,:language)");
-		$varis = array (":userID"=>$userID, ":genre"=>$genre, ":duration"=>$duration, ":year"=>$year, ":language"=>$language);
+		$db = new PDO("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword); // get into db
+
+		$fInsert = $db->prepare("INSERT INTO `formTable` (`userID`, `genre`, `duration`, `year`, `language`) 
+		values (:userID, :genre, :duration, :era,:langu)");
+		$varis = array(":userID" => $userID, ":genre" => $genre, ":duration" => $duration, ":era" => $year, ":langu" => $language);
 		$fInsert->execute($varis);
-		
 	} catch (Exception $e) {
-		echo "you were not able to save your prefs :( ". $e->getMessage(). PHP_EOL;	
+		echo "you were not able to save your prefs :( " . $e->getMessage() . PHP_EOL;
 	}
-
-
 }
 
 
-	// function for adding movies to the db from the api (adnrew will need this I think)
-	// lol he didnt but good warm up
-function setMovie ($imbd_id, $title, $desc, $image, $genre, $duration, $year, $language){
+// function for adding movies to the db from the api (adnrew will need this I think)
+// lol he didnt but good warm up
+// this is just to popular the table .. and i might end up using this too LOL
+function setMovie($imbd_id, $title, $desc, $image, $genre, $duration, $year, $language)
+{
 	try {
 		require 'config.php'; // to get the sensitive stuff
-		$db = new PDO ("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword); // get into db
-		
-		$fInsert = $db->prepare("INSERT INTO `movies` (`imdb_id`, `title`, `description`, `image`, genre, duration, `year`, `language`)
-		values ($imbd_id, $title, $desc, $image, $genre, $duration, $year, $language)");
-		$x = array (":imdb_db"=>$imbd_id, ":title"=>$title, ":desc"=>$desc, ":image"=> $image, ":genre"=>$genre, ":duration"=>$duration, ":year"=>$year, ":language"=>$language);
+		$db = new PDO("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword); // get into db
+
+		$fInsert = $db->prepare("INSERT INTO `movies` (`imdb_id`, `title`, `description`, `image`, `genre`, `duration`, `year`, `language`)
+		values (:imdb_db, :title, :descp, :img, :genre, :duration, :era, :langu);");
+		$x = array(":imdb_db" => $imbd_id, ":title" => $title, ":descp" => $desc, ":img" => $image, ":genre" => $genre, ":duration" => $duration, ":era" => $year, ":langu" => $language);
 		$fInsert->execute($x);
-		
+		return true;
 	} catch (Exception $e) {
-		echo "you were not able to cache ur movies( ". $e->getMessage(). PHP_EOL;	
-	} 
-
-
-
-
+		echo "you were not able to cache ur movies( " . $e->getMessage() . PHP_EOL;
+	}
 }
 
+// create a werid string for sessions
+function generateRandomString()
+{
+	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$charactersLength = strlen($characters);
+	$String = '';
+	for ($i = 0; $i < 15; ++$i) {
+		$String .= $characters[rand(0, $charactersLength - 1)];
+	}
+	return $String;
+}
 
-
-?>
+// to add stuff to session table
+function setSession($userID)
+{
+	try {
+		require 'config.php'; // to get the sensitive stuff
+		$db = new PDO("mysql:host=$servername;dbname=FourTestingP", $dbusername, $dbpassword);
+		$seed = generateRandomString();
+		$cTime = time();
+		$abc = $db->prepare("INSERT INTO `userSession` (`session_id`,`user_id`,`creation`) 
+		values (:sesid, :usrid, :djtime)");
+		$v = array(":sesid" => $seed, ":usrid" => $userID, ":djtime" => $cTime);
+		$abc->execute($v);
+	} catch (Exception $e) {
+		echo "this is quite unfortunate. You cant add users at the momment. :(" . $e->getMessage();
+	}
+}
---
+++ Yoink/schema/creation/createTables.php
@@ -0,0 +1,141 @@
+<?php
+
+
+/*
+    JNCV
+   Using this, you can create the tables. 
+   I will be using the create if not exist, so that if the script runs, it will not make tables that are already in the server LOL
+   also if you run it at first, it will create all the tables that are needed.
+   
+   
+*/
+
+
+// get the information from the config files
+
+
+try {
+
+    require("../creds.php"); // this will take care of which db is being used
+    $db = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+
+
+
+    // test code
+    /*
+
+    $stmt = $db->prepare("CREATE TABLE IF NOT EXISTS `ProjectsTable` 
+        (
+        `id` int auto_increment not null,
+        `username` varchar(30) not null unique,
+        `pwrd` varchar (200) not null,
+        PRIMARY KEY (`id`)
+        )
+         CHARACTER SET utf8 COLLATE utf8_general_ci"
+    );
+
+    //remember to execute what you prepare!
+    $stmt->execute();
+    */
+
+    // creating table for userLogin
+    $createUserTable = $db->prepare(
+        "CREATE TABLE IF NOT EXISTS `userLogin`
+        (
+        `userID` int auto_increment not null,
+        `username` varchar (50) not null unique,
+        `password` varchar (255) not null,
+        `email` varchar (200) not null,
+        primary key (`userID`)
+        )"
+
+    );
+    $createUserTable->execute(); // this will run the code above if it can
+    // end of creation of userLogins 
+
+    // start of create of form table
+    $createFormTable = $db->prepare(
+        "CREATE TABLE IF NOT EXISTS formTable(
+            `userID` int not null,
+            `genre` varchar (100),
+            `duration` int not null,
+            `year` varchar (20),
+            `language` varchar (200),
+            Primary Key (`userID`),
+            Foreign Key (`userID`) references userLogin(`userID`)
+        );"
+    );
+
+    $createFormTable->execute();
+    // end of creation of form table
+
+    // creation of userSession
+    $createUserSes = $db->prepare(
+        "CREATE TABLE IF NOT EXISTS userSession(
+        `session_id` varchar (255),
+        `user_id` int not null,
+        `creation` int 
+        );"
+
+    );
+
+    $createUserSes->execute(); // this will run the code above if it can
+    // end of isnta
+
+
+    //create of cachedMovie Table (for all movies :))
+    $cm = $db->prepare("CREATE TABLE IF NOT EXISTS `movies`(
+          `movie_id` int auto_increment,
+          `imdb_id` varchar (12) not null,
+          `title` varchar (200) not null,
+          `description` text,
+          `image` text,
+          `genre` varchar(200),
+          `duration` int (10),
+          `year` int (10),
+          `language` varchar (200),
+          Primary Key (movie_id)
+        );
+    ");
+    $cm->execute();    // tnd of create cachedMovie
+
+
+    // start of userInventory
+    $ui = $db->prepare("CREATE TABLE IF NOT EXISTS `userInv`(
+            `movie_id` int not null,
+            `userID`int not null,
+            Foreign Key (`userID`) references userLogin(`userID`),
+            Foreign Key (`movie_id`) references movies(`movie_id`)     
+            );");
+
+    $ui->execute(); // end of userInventory 
+
+
+    // creating review table
+    $rc = $db->prepare("CREATE TABLE IF NOT EXISTS `reviews`(
+        `review_id` int auto_increment,
+        `movie_id` int not null,
+        `userID` int not null,
+        `reviewRating` int not null, 
+        `reviewText` text,    
+        Primary Key (review_id),
+        Foreign Key (`userID`) references userLogin(`userID`),
+        Foreign Key (`movie_id`) references movies(`movie_id`)     
+        );
+    ");
+
+    $rc->execute(); // end of userInventory 
+
+
+
+
+
+
+
+
+} catch (Exception $e) {
+
+    echo $e->getMessage();
+    echo "it didnt work LMAO";
+    exit('SOMETHING BROKE :(');
+}
